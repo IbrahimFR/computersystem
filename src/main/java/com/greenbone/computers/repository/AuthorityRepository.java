4e556f8e94210e72cdd5633a6b816699d15c0894
IbrahimFR <ibrahimfouad66@gmail.com>
2023-05-03 05:32:12 +0300

    Initial commit

---
+++ src/main/java/com/greenbone/computers/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.greenbone.computers.repository;
+
+import com.greenbone.computers.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/greenbone/computers/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.greenbone.computers.web.rest.vm;
